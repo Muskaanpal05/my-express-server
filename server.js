15b6432707c6ed680f5bb2c243703130569a88ec
Muskaanpal05 <palmuskaan5@gmail.com>
2020-10-15 13:11:13 +0530

    Working with Routes

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ app.get("/", function(req, res) {
     res.send("<h1><em>Hello there!</em></h1>");
 });
 
+app.get("/contact", function(req, res) {
+    res.send("Contact me at: palmuskaan5@gmail.com");
+});
+
+app.get("/about", function(req, res) {
+    res.send("My name is Muskaan Pal studying at Chitkara University.");
+});
+
+app.get("/hobbies", function(req, res) {
+    res.send("<ul type= 'square'><li>Cooking</li><li>Dancing</li></ul>");
+});
+
+app.get("/skills", function(req, res) {
+    res.send("<ul type = 'square'><li><em>HTML</em></li><li><em>CSS</em></li><li><em>Javascript</em></li></ul>");
+});
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
